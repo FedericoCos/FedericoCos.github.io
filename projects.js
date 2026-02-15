b37f93ca4b02cdebe35f8ddabdee8da436fa156c
Federico Costantini <fedecosta1234@gmail.com>
2026-02-15 19:09:51 +0100

    Phone improvements (video boxing)

--- projects.js
+++ projects.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let isFreezed = false;
     let hasMoved = false;
     let startX;
-    const speed = 1.5;
+    const speed = window.matchMedia("(max-width: 768px)").matches ? 1.0 : 1.5;
     const gap = 20;
 
     const originalCards = [...inner.children];
